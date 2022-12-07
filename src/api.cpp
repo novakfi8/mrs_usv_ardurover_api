05a4209fee015c586f1b09474fe0c091dc3a3423
Tomas Baca <klaxalk@gmail.com>
2022-12-07 16:07:20 +0100

    compilable

--- src/api.cpp
+++ src/api.cpp
@@ -2,6 +2,8 @@
 
 #include <ros/ros.h>
 
+#include <mrs_uav_hw_api/api.h>
+
 #include <nav_msgs/Odometry.h>
 
 #include <mrs_lib/param_loader.h>
@@ -15,46 +17,38 @@
 namespace mrs_uav_pixhawk_api
 {
 
-/* class ControllerModule //{ */
+/* class HwApi //{ */
 
-class HwApi : public mrs_uav_managers::Controller {
+class HwApi : public mrs_uav_hw_api::MrsUavHwApi {
 
 public:
   ~HwApi(){};
 
-  void initialize(const ros::NodeHandle &parent_nh, const std::string name, const std::string name_space, const double uav_mass,
-                  std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers);
-  bool activate(const mrs_msgs::AttitudeCommand::ConstPtr &last_attitude_cmd);
-  void deactivate(void);
+  void initialize(const ros::NodeHandle &parent_nh, std::shared_ptr<mrs_uav_hw_api::CommonHandlers_t> common_handlers, const std::string &topic_prefix, const std::string &uav_name);
 
-  const mrs_msgs::AttitudeCommand::ConstPtr update(const mrs_msgs::UavState::ConstPtr &uav_state, const mrs_msgs::PositionCommand::ConstPtr &control_reference);
-  const mrs_msgs::ControllerStatus          getStatus();
+  mrs_msgs::HwApiDiagnostics getDiagnostics();
+  mrs_msgs::HwApiMode        getMode();
 
-  void switchOdometrySource(const mrs_msgs::UavState::ConstPtr &new_uav_state);
+  // | --------------------- topic callbacks -------------------- |
 
-  void resetDisturbanceEstimators(void);
+  bool callbackControlGroupCmd(const mrs_msgs::HwApiControlGroupCmd &msg);
 
-  const mrs_msgs::DynamicsConstraintsSrvResponse::ConstPtr setConstraints(const mrs_msgs::DynamicsConstraintsSrvRequest::ConstPtr &cmd);
+  bool callbackAttitudeRateCmd(const mrs_msgs::HwApiAttitudeRateCmd &msg);
+
+  bool callbackAttitudeCmd(const mrs_msgs::HwApiAttitudeCmd &msg);
+
+  bool callbackTranslationCmd(const mrs_msgs::HwApiTranslationCmd &msg);
+
+  // | -------------------- service callbacks ------------------- |
+
+  std::tuple<bool, std::string> callbackArming(const bool &request);
+
+  std::tuple<bool, std::string> callbackOffboard(const bool &request);
 
 private:
   bool is_initialized_ = false;
-  bool is_active_      = false;
 
-  std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers_;
-
-  double _uav_mass_;
-
-  double hover_thrust_;
-
-  // | --------------- dynamic reconfigure server --------------- |
-
-  boost::recursive_mutex                             mutex_drs_;
-  typedef controller_module::controller_paramsConfig DrsParams_t;
-  typedef dynamic_reconfigure::Server<DrsParams_t>   Drs_t;
-  boost::shared_ptr<Drs_t>                           drs_;
-  void                                               callbackDrs(controller_module::controller_paramsConfig &params, uint32_t level);
-  DrsParams_t                                        params_;
-  std::mutex                                         mutex_params_;
+  std::shared_ptr<mrs_uav_hw_api::CommonHandlers_t> common_handlers_;
 };
 
 //}
@@ -65,16 +59,15 @@ private:
 
 /* initialize() //{ */
 
-void HwApi::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const std::string name, const std::string name_space,
-                                  const double uav_mass, std::shared_ptr<mrs_uav_managers::CommonHandlers_t> common_handlers) {
+void HwApi::initialize(const ros::NodeHandle &parent_nh, std::shared_ptr<mrs_uav_hw_api::CommonHandlers_t> common_handlers, const std::string &topic_prefix, const std::string &uav_name) {
 
-  ros::NodeHandle nh_(parent_nh, name_space);
+  ros::NodeHandle nh_(parent_nh);
 
   common_handlers_ = common_handlers;
 
   // | ------------------- loading parameters ------------------- |
 
-  /* mrs_lib::ParamLoader param_loader(nh_, "ControllerModule"); */
+  mrs_lib::ParamLoader param_loader(nh_, "MrsUavPixhawkApi");
 
   if (!param_loader.loadedSuccessfully()) {
     ROS_ERROR("[ControllerModule]: Could not load all parameters!");
@@ -90,7 +83,7 @@ void HwApi::initialize(const ros::NodeHandle &parent_nh, [[maybe_unused]] const 
 
 //}
 
-}  // namespace controller_module
+}  // namespace mrs_uav_pixhawk_api
 
 #include <pluginlib/class_list_macros.h>
 PLUGINLIB_EXPORT_CLASS(mrs_uav_pixhawk_api::HwApi, mrs_uav_hw_api::MrsUavHwApi)
